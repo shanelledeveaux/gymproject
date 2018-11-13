23012fafee67a907719ff08318060aad3bc292e9
Shanelle Webster <webstershanelle@gmail.com>
2018-11-12 21:20:22 -0600

    home page layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header/Header";
 const App = () => {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       {routes}
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Home.css";
+import gardenrunner from "./gardenrunner.png";
+import downarrow from "./downarrow.png";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="title_box">
+        <h1>Shanelle's Gym</h1>
+        <div>Strong is the new beautiful</div>
+        <p>About</p>
+        <p>Shop</p>
+        <p>Join</p>
+      </div>
+      <img src={gardenrunner} alt="" />
+    </div>
+  );
 };
 export default Home;
