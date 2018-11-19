9a2e9e863d10c2a79f5a4fc377564182cba7f9c0
Shanelle Webster <webstershanelle@gmail.com>
2018-11-19 13:51:31 -0600

    starting on calc

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
+import "./Calculator.css";
 import MacroModal from "./MacroModal/MacroModal";
 
 const Calculator = () => {
-  const initialState = { modal: true };
+  const userData = [];
 
-  const [modalAppearance] = useState(initialState);
+  const [modalAppearance, setModal] = useState(false);
 
+  const [users, setUser] = useState(userData);
+
+  const addUser = user => {
+    setUser([...users, user]);
+    setModal(false);
+  };
+
+  console.log("user data", users);
   return (
-    <div>
-      Calculator
-      {modalAppearance.modal === true ? <MacroModal /> : null}
+    <div className="calculator">
+      {modalAppearance === true ? <MacroModal addUser={addUser} /> : null}
+      <div className="calculator_information">
+        <h2>User's Metabolic Information and Suggested Calorie Intake</h2>
+      </div>
     </div>
   );
 };
--- src/components/Calculator/MacroModal/MacroModal.js
+++ src/components/Calculator/MacroModal/MacroModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./MacroModal.css";
 
-const MacroModal = () => {
+const MacroModal = props => {
   const initialFormState = {
     gender: "female",
     age: 0,
@@ -24,170 +24,177 @@ const MacroModal = () => {
     <div>
       <div className="modal_background" />
       <div className="modal">
-        Form
-        <form>
-          <div className="macro_calc_form">
-            <div className="calc_section">
-              <label>Gender</label>
-              <div className="calc_choice">
-                <div>
-                  <input
-                    type="radio"
-                    name="gender"
-                    value="male"
-                    onChange={handleChange}
-                  />
-                  Male
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    name="gender"
-                    value="female"
-                    onChange={handleChange}
-                  />
-                  Female
-                </div>
-              </div>
-            </div>
-            <div>
-              <input
-                name="age"
-                className="field_input"
-                placeholder="Age"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="calc_section">
-              <label>Height</label>
-              <div className="calc_choice">
-                <div>
-                  <input
-                    name="feet"
-                    className="field_input"
-                    placeholder="Feet"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <div>
-                  <input
-                    name="inches"
-                    className="field_input"
-                    placeholder="Inches"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-              </div>
-            </div>
-            <div>
-              <input
-                name="weight"
-                className="field_input"
-                placeholder="Weight"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Describe Your Normal Daily Activity</label>
+        Macro Calculator
+        <div className="macro_calc_form">
+          <div className="calc_section">
+            <label>Gender</label>
+            <div className="calc_choice">
               <div>
                 <input
                   type="radio"
-                  name="activity"
-                  value="1.2"
+                  name="gender"
+                  value="male"
                   onChange={handleChange}
                 />
-                Sedentary - Desk Job
+                Male
               </div>
               <div>
                 <input
                   type="radio"
-                  name="activity"
-                  value="1.375"
+                  name="gender"
+                  value="female"
                   onChange={handleChange}
                 />
-                Lightly Active - Climb Stairs A Few Times Per Day
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  name="activity"
-                  value="1.55"
-                  onChange={handleChange}
-                />
-                Moderately Active - Teacher, Salesman, Etc.
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  name="activity"
-                  value="1.725"
-                  onChange={handleChange}
-                />
-                Very Active - Carpenter, Mailman, Etc.
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  name="activity"
-                  value="1.9"
-                  onChange={handleChange}
-                />
-                Extremely Active - Soldier, Boxer, Etc.
+                Female
               </div>
             </div>
-            <div className="calc_section">
-              <label>Goals</label>
-              <div className="calc_choice">
-                <div>
-                  <input
-                    type="radio"
-                    name="goal"
-                    value="loss"
-                    onChange={handleChange}
-                  />
-                  Weight Loss
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    name="goal"
-                    value="gain"
-                    onChange={handleChange}
-                  />
-                  Weight Gain
-                </div>
-              </div>
-            </div>
-            {/* CHANGE THIS TO REF/LECT STATE; */}
-            {user.goal === "loss" ? (
-              <div className="calc_section">
-                Calorie Deficit:
-                <div className="selectbox">
-                  <select name="deficit" onChange={handleChange}>
-                    <option value=".2">20% Suggested</option>
-                    <option value=".25">25% Aggressive</option>
-                    <option value=".3">30% Intense</option>
-                  </select>
-                </div>
-              </div>
-            ) : (
-              <div className="calc_section">
-                Calorie Surplus:
-                <div className="selectbox" onChange={handleChange}>
-                  <select name="deficit">
-                    <option value="1.2">20% Suggested</option>
-                    <option value="1.25">25% Aggressive</option>
-                    <option value="1.3">30% Intense</option>
-                  </select>
-                </div>
-              </div>
-            )}
           </div>
-        </form>
+          <div>
+            <input
+              name="age"
+              className="field_input"
+              placeholder="Age"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="calc_section">
+            <label>Height</label>
+            <div className="calc_choice">
+              <div>
+                <input
+                  name="feet"
+                  className="field_input"
+                  placeholder="Feet"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  name="inches"
+                  className="field_input"
+                  placeholder="Inches"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            <input
+              name="weight"
+              className="field_input"
+              placeholder="Weight"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Describe Your Normal Daily Activity</label>
+            <div>
+              <input
+                type="radio"
+                name="activity"
+                value="1.2"
+                onChange={handleChange}
+              />
+              Sedentary - Desk Job
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="activity"
+                value="1.375"
+                onChange={handleChange}
+              />
+              Lightly Active - Climb Stairs A Few Times Per Day
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="activity"
+                value="1.55"
+                onChange={handleChange}
+              />
+              Moderately Active - Teacher, Salesman, Etc.
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="activity"
+                value="1.725"
+                onChange={handleChange}
+              />
+              Very Active - Carpenter, Mailman, Etc.
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="activity"
+                value="1.9"
+                onChange={handleChange}
+              />
+              Extremely Active - Soldier, Boxer, Etc.
+            </div>
+          </div>
+          <div className="calc_section">
+            <label>Goals</label>
+            <div className="calc_choice">
+              <div>
+                <input
+                  type="radio"
+                  name="goal"
+                  value="loss"
+                  onChange={handleChange}
+                />
+                Weight Loss
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="goal"
+                  value="gain"
+                  onChange={handleChange}
+                />
+                Weight Gain
+              </div>
+            </div>
+          </div>
+          {user.goal === "loss" ? (
+            <div className="calc_section">
+              Calorie Deficit:
+              <div className="selectbox">
+                <select name="deficit" onChange={handleChange}>
+                  <option value=".2">20% Suggested</option>
+                  <option value=".25">25% Aggressive</option>
+                  <option value=".3">30% Intense</option>
+                </select>
+              </div>
+            </div>
+          ) : (
+            <div className="calc_section">
+              Calorie Surplus:
+              <div className="selectbox" onChange={handleChange}>
+                <select name="deficit">
+                  <option value="1.2">20% Suggested</option>
+                  <option value="1.25">25% Aggressive</option>
+                  <option value="1.3">30% Intense</option>
+                </select>
+              </div>
+            </div>
+          )}
+        </div>
+        <button
+          onClick={event => {
+            // event.preventDefault();
+            // if (!user.age || !user.weight) return;
+
+            props.addUser(user);
+          }}
+        >
+          Add User
+        </button>
       </div>
     </div>
   );
